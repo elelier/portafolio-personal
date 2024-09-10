2fe2063b8ff1122e67c4523001872a499c269402
elelier <loya.elier@gmail.com>
2024-09-10 16:46:40 -0600

    banner

--- my-app/src/components/Contacto.js
+++ my-app/src/components/Contacto.js
@@ -5,7 +5,6 @@ import CV from './assets/files/2409_CV_EL.pdf';
 import { motion } from 'framer-motion';
 import './css/Contacto.css';
 import perfilImg from './assets/files/profile-picture-elier2.png';
-import banner from './assets/files/mail-banner-1.png';
 
 function Contacto() {
   const [name, setName] = useState('');
