a6515f3a5318bfa7d03a8122a61c59e1004b7f42
elelier <loya.elier@gmail.com>
2025-06-12 17:31:13 -0600

    chore: update deploy scripts and fix router

--- my-app/src/config/hashedPasscodes.js
+++ my-app/src/config/hashedPasscodes.js
@@ -1,8 +1,8 @@
 // ARCHIVO GENERADO AUTOMÁTICAMENTE - NO EDITAR MANUALMENTE
-// Generado el: 2025-06-12T22:32:19.810Z
+// Generado el: 2025-06-12T23:21:17.996Z
 
 export const hashedPasscodes = {
-  "arqidia": "7046efc2af8e6b65e2b39654f8cf7c49c053bc360f0bbb749d197ef26e8fef64$salt$3.0$6089155cb52945bced6cf58cee0a6f32",
-  "hqcontrol": "92f161ed43c9d4b620a82f54587fb38ed9c6a4d2c605781386088a69a18eb1a2$salt$3.0$d930282be6f6c45295ab2d30eb5f6d3d",
-  "andamios": "22d439c04366c1cb01326f41d7e43d3020765e8ec82e2e807efaa3747391c5ed$salt$3.0$ebe66cb1a2ea4afc88f0cb5aa6cc2a95"
+  "arqidia": "b05753f2877125c7deca79b50e0772b1f14a437b184655baef351a3dd844671e$salt$3.0$63d7a7f89cb514b403660413b742b155",
+  "hqcontrol": "bc2cc953d257b2277a27bf4d30c1465fdac3ccbf602715edeb765e817fb357e3$salt$3.0$d346e1884afcf4af54be69a381019533",
+  "andamios": "6c2aeb0b397e698944b4ce24477b5d6ae4660510d4766bf5114e0ef71c28a634$salt$3.0$571addb7b12b82e20c6696aec919234d"
 };
