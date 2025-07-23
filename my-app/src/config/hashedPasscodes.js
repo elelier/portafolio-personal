3968c3c573f97743f89ce6bd6ebd026515684218
elelier <loya.elier@gmail.com>
2025-07-22 19:44:20 -0600

    update: hashed passcodes and client projects data

--- my-app/src/config/hashedPasscodes.js
+++ my-app/src/config/hashedPasscodes.js
@@ -1,8 +1,8 @@
 // ARCHIVO GENERADO AUTOMÁTICAMENTE - NO EDITAR MANUALMENTE
-// Generado el: 2025-07-15T20:15:57.631Z
+// Generado el: 2025-07-15T20:16:43.335Z
 
 export const hashedPasscodes = {
-  "arqidia": "7d93b8c6facf4b29a853f0b8456ea856f37fd3e0af00fb0b894b05a60b1182e8$salt$3.0$dde83979b3e23951e9a100fa6dc7e586",
-  "hqcontrol": "7712ed64b609dd5b9b735c647ba6559aefc86612d27fdff40deb0cf065d48751$salt$3.0$85b8e062216113f5ae8b0264c7c2da0a",
-  "andamios": "ed5d24b30af968a6af0100b796e37348d1ffa75301e6ac55debec1bac7f23554$salt$3.0$e81e5c9363743feb296eea278ad45017"
+  "arqidia": "eab4e224dfc8442fd1c6df20f9be9d41267ab4b24413732b000e396ba9c60b84$salt$3.0$e83aa0e8ab0abfc3360883fb90515dad",
+  "hqcontrol": "31fd84a2a5e8ab5938713f7b84abe60aaff4cf36f5e3eecd1d7a08445b6c1a3d$salt$3.0$a4e05398b9607a23922cc1baa50c87f8",
+  "andamios": "7b8bf66ac25d124c1346793304c794d9aa309dcab717dc5fb1776df1c8af334d$salt$3.0$6132ce8b72dae0a2f9dc3ee47031bada"
 };
